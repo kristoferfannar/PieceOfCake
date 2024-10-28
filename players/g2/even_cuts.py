c22f2dcea5933b32161ae4e42eb40f1efe5349f6
hanita <hhaller@wisc.edu>
2024-10-28 16:46:24 -0400

    tidy even cuts

--- players/g2/even_cuts.py
+++ players/g2/even_cuts.py
@@ -3,7 +3,7 @@ import numpy as np
 from players.g2.helpers import *
 
 class EvenCuts:
-    def __init__(self, num_requests, cake_width, cake_len):
+    def __init__(self, requests, cake_width, cake_len):
         self.phase = "HORIZONTAL"
         self.direction = ""
         self.n = num_requests
@@ -13,63 +13,58 @@ class EvenCuts:
         self.s_y = cake_len / np.sqrt(self.n)
         self.move_queue = []
 
+    def horizontal_to_vertical(self, pos):
+        self.phase = "VERTICAL"
+        if pos[0] != 0:
+            self.s_x = 0 - self.s_x
+        self.move_queue.extend(sneak(pos, [pos[0] + self.s_x, self.cake_len], self.cake_width, self.cake_len))
+        self.move_queue.append([pos[0] + self.s_x, 0])
+
+    def horizontal_cut(self, pos):
+        self.move_queue.extend(sneak(pos, [pos[0], pos[1] + self.s_y], self.cake_width, self.cake_len))
+        if pos[0] == 0:
+            opposite = self.cake_width
+        else:
+            opposite = 0
+        self.move_queue.append([opposite, round(pos[1] + self.s_y, 2)])
+
+    def vertical_cut(self, pos):
+        self.move_queue.extend(sneak(pos, [pos[0] + self.s_x, pos[1]], self.cake_width, self.cake_len))
+        if pos[1] == 0:
+            opposite = self.cake_len
+        else:
+            opposite = 0
+        self.move_queue.append([new_x, opposite])
+
     def even_cuts(self, pos):
-            """
-            Adds moves to the merge queue that will cut the cake into even slices.
-            """
-
-            if self.phase == "HORIZONTAL" and pos[1] + self.s_y >= self.cake_len:
-                self.phase = "VERTICAL"
-                if pos[0] == 0:
-                    new_x = self.s_x
-                else:
-                    new_x = self.cake_width - self.s_x
-                    self.direction = "RIGHT"
-                self.move_queue.extend(sneak(pos, [new_x, self.cake_len], self.cake_width, self.cake_len))
-                self.move_queue.append([new_x, 0])
-
-                return
-
-            if self.phase == "HORIZONTAL":
-                self.move_queue.extend(sneak(pos, [pos[0], pos[1] + self.s_y], self.cake_width, self.cake_len))
-                if pos[0] == 0:
-                    opposite = self.cake_width
-                else:
-                    opposite = 0
-                self.move_queue.append([opposite, round(pos[1] + self.s_y, 2)])
-
+        """
+        Adds moves to the merge queue that will cut the cake into even slices.
+        """
+        if self.phase == "HORIZONTAL":
+            if pos[1] + self.s_y < self.cake_len:
+                self.horizontal_cut(pos)
             else:
-                if self.direction == "RIGHT":
-                    new_x = pos[0] - self.s_x
-                else:
-                    new_x = pos[0] + self.s_x
-
-                if new_x <= 0 or new_x >= self.cake_width:
-                    self.phase = "DONE"
-                    return
-
-                self.move_queue.extend(sneak(pos, [new_x, pos[1]], self.cake_width, self.cake_len))
-                if pos[1] == 0:
-                    opposite = self.cake_len
-                else:
-                    opposite = 0
-                self.move_queue.append([new_x, opposite])
-
-            return
+                self.horizontal_to_vertical(pos)
+        else:
+            if pos[0] + self.s_x in range(0.01, self.cake_width):
+                self.vertical_cut(pos)
+            else:
+                self.phase = "DONE"
+        return
 
     def move(self, turn_number, cur_pos):
-            if turn_number == 1:
-                return constants.INIT, [0.01, 0]
-            
-            if turn_number == 2:
-                self.move_queue.append([0, self.s_y])
-                self.move_queue.append([self.cake_width, self.s_y])
-            elif len(self.move_queue) == 0 and self.phase != "DONE":
-                self.even_cuts(cur_pos)
+        if turn_number == 1:
+            return constants.INIT, [0.01, 0]
+        
+        if turn_number == 2:
+            self.move_queue.append([0, self.s_y])
+            self.move_queue.append([self.cake_width, self.s_y])
+        elif len(self.move_queue) == 0 and self.phase != "DONE":
+            self.even_cuts(cur_pos)
 
-            if len(self.move_queue) > 0:
-                next_val = self.move_queue.pop(0)
-                cut = [round(next_val[0], 2), round(next_val[1], 2)]
-                return constants.CUT, cut
+        if len(self.move_queue) > 0:
+            next_val = self.move_queue.pop(0)
+            cut = [round(next_val[0], 2), round(next_val[1], 2)]
+            return constants.CUT, cut
 
-            return None
+        return None
